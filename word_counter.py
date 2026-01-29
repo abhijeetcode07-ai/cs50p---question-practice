01f26468c37d39731ee2e3e4494d825fa4520560
abhijeetcode07-ai <abhijeet.code07@gmail.com>
2026-01-29 15:29:17 +0530

    Removed duplicate word_counter file

--- word_counter.py
+++
@@ -1,7 +0,0 @@
-def count_word() :
-    sent = input("Enter you sentence :  ")
-    word = sent.split()
-    return len(word)
-
-result = count_word()
-print("Total number of words in the sentence is :  ",result)
